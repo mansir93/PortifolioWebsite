c6bd23e32c4812ffb9e345f06b1a33d7ac1176a9
Mansir <mansiraziz93@gmail.com>
2023-05-11 04:50:37 +0000

    fix bugs

--- src/component/ContactMe/Form.js
+++ src/component/ContactMe/Form.js
@@ -29,7 +29,7 @@ const Form = () => {
       message: message.value,
     };
 
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://mailserver-api.vercel.app/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
